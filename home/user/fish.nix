9e51d889fb7b924e84fcdb92ca5a7330d28c8eec
YukihiroArakawa <yukiburos@gmail.com>
2026-06-07 17:04:27 +0900

    feat: add git abbr

--- home/user/fish.nix
+++ home/user/fish.nix
@@ -5,6 +5,9 @@
     enable = true;
     shellAbbrs = {
       nrs = "sudo nixos-rebuild switch --flake .#nixos";
+      # git abbr
+      gs = "git status";
+      gpoh = "git push origin HEAD";
     };
     interactiveShellInit = ''
       set fish_greeting
