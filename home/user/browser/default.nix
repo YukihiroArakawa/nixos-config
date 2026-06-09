e01ae60dc28e0e7616561d0eb4451afd1d6b79a0
YukihiroArakawa <yukiburos@gmail.com>
2026-06-09 23:39:43 +0900

    feat: add firefox with keeper plugins

--- home/user/browser/default.nix
+++ home/user/browser/default.nix
@@ -4,4 +4,15 @@
   home.packages = [
     pkgs.brave
   ];
+
+  programs.firefox = {
+    enable = true;
+
+    policies.ExtensionSettings = {
+      "KeeperFFStoreExtension@KeeperSecurityInc" = {
+        installation_mode = "normal_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/keeper-password-manager/latest.xpi";
+      };
+    };
+  };
 }
