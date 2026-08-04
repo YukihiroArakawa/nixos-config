32d0786d74f578b52219d15c4c85fb2fd43331d3
YukihiroArakawa <yukiburos@gmail.com>
2026-08-04 22:32:12 +0900

    chore(tmux): add blind75 window

--- home/user/tmux/tmux_setup.sh
+++ home/user/tmux/tmux_setup.sh
@@ -12,6 +12,8 @@ if ! tmux has-session -t "$session_name" 2>/dev/null; then
 
   tmux new-window -t "$session_name:1" -n "blogs" -c "$workspace/yukiotechblog"
 
+  tmux new-window -t "$session_name:2" -n "blind75" -c "$workspace/blind75"
+
   tmux select-window -t "$session_name:0"
 fi
 
