b56f8dee20d86aaaaa66d44b717e4597b4c1b5a2
YukihiroArakawa <yukiburos@gmail.com>
2026-09-21 10:56:34 +0900

    build(nixos): enable fingerprint authentication

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -78,6 +78,7 @@
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+  services.fprintd.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
